fe6bdf6d5178b7b21ac575746541074e8a7fe083
zlw8844 <zlw8844@gmail.com>
2024-07-09 16:30:31 -0700

    proto update

--- protoc.sh
+++ protoc.sh
@@ -11,7 +11,7 @@ git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
 #git fetch --all && git checkout main
 
-sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
+sudo brew install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=./golang --go_opt=paths=source_relative \
